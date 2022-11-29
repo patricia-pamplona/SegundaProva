7b2990bc640daa3df7df966da2e54c06e4b97a56
iZezito <emersonsilva81240@gmail.com>
2022-11-29 19:56:53 -0300

    questao 5

---
+++ app/src/main/java/com/example/segundaprova/converters/DataBindingConverters.kt
@@ -0,0 +1,22 @@
+package com.example.segundaprova.converters
+
+import android.text.TextUtils
+import androidx.databinding.InverseMethod
+
+class DataBindingConverters {
+    companion object {
+        @InverseMethod("convertIntToString")
+        @JvmStatic
+        fun convertStringToInt(value: String): Int {
+            if (TextUtils.isEmpty(value) || !TextUtils.isDigitsOnly(value)) {
+                return 0
+            }
+            return value.toInt()
+        }
+
+        @JvmStatic
+        fun convertIntToString(value: Float?): String {
+            return value?.toString() ?: ""
+        }
+    }
+}
--- app/src/main/java/com/example/segundaprova/fragments/home/cadastro/CadastraViewModel.kt
+++ app/src/main/java/com/example/segundaprova/fragments/home/cadastro/CadastraViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.segundaprova.fragments.home.cadastro
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.segundaprova.dominio.PersonagemFilme
+import com.example.segundaprova.repository.PersonagemFilmeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 @HiltViewModel
-class CadastraViewModel:ViewModel() {
+class CadastraViewModel @Inject constructor(private var repository: PersonagemFilmeRepository) : ViewModel() {
+    var personagemFilme = MutableLiveData<PersonagemFilme>()
+
+    init {
+        personagemFilme.value = PersonagemFilme("","","","",0)
+    }
+
+    fun cadastrarButtonEvent(){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                personagemFilme.value?.let { repository.create(it) }
+            }
+        }
+    }
 }
