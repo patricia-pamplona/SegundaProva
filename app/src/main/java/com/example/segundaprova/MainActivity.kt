76f62b23283f8f89a0a43a37fc808d5067b2037f
iZezito <emersonsilva81240@gmail.com>
2022-12-03 15:05:56 -0300

    90%

--- app/src/main/java/com/example/segundaprova/MainActivity.kt
+++ app/src/main/java/com/example/segundaprova/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.homeFragment, R.id.cadastraFragment
+                R.id.homeFragment, R.id.cadastraFragment, R.id.sobreFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
